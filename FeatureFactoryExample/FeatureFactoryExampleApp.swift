37a835267b6fa5ea86cbaef10a9296c22e5bfd3a
Mikalai Zmachynski <nikolai.zmachinsky@gemspace.com>
2026-05-01 08:17:49 +0200

    Initial Commit

---
+++ FeatureFactoryExample/FeatureFactoryExampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  FeatureFactoryExampleApp.swift
+//  FeatureFactoryExample
+//
+//  Created by Mikalai Zmachynski on 01/05/2026.
+//
+
+import SwiftUI
+
+@main
+struct FeatureFactoryExampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
